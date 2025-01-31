e4627acdd3eaf0f6469290fac45ad6af0851f7a0
SergeAbramyan <t9856412966@mail.ru>
2025-02-01 02:11:52 +0400

    2025/01/31

--- src/main/java/cohort46/gracebakeryapi/bakery/bakeryoptional/dao/BakeryoptionalRepository.java
+++ src/main/java/cohort46/gracebakeryapi/bakery/bakeryoptional/dao/BakeryoptionalRepository.java
@@ -1,7 +1,6 @@
 package cohort46.gracebakeryapi.bakery.bakeryoptional.dao;
 
 import cohort46.gracebakeryapi.bakery.bakeryoptional.model.Bakeryoptional;
-import cohort46.gracebakeryapi.bakery.category.model.Category;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
