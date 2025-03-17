b3522329c86b2eb5a3be1f917b42c9eaa76d557e
SergeAbramyan <t9856412966@mail.ru>
2025-03-17 09:52:35 +0400

    2025/02/20

--- src/main/java/cohort46/gracebakeryapi/accounting/security/GlobalExceptionHandler.java
+++ src/main/java/cohort46/gracebakeryapi/accounting/security/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 import java.util.Collections;
 
@@ -16,4 +17,11 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                 .body(Collections.singletonMap("error", "Invalid username or password"));
     }
+
+    @ExceptionHandler(RuntimeException .class)
+    public ResponseEntity<?> handleCustomRuntimeException(RuntimeException  ex, WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+
 }
